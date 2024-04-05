e41152b9b7390f744c6edec443d841eebaeb2e11
Tolu A <mrtalder@gmail.com>
2024-04-05 18:23:44 +0100

    Create First Cell in the Grid Container

---
+++ script.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector('.grid');
+const gridCell = document.createElement('div');
+
+gridCell.style.width = '16px';
+gridCell.style.height = '16px';
+gridCell.style.borderStyle = 'solid';
+gridCell.style.borderColor = '#909090';
+gridCell.style.borderWidth = '1px';
+
+grid.appendChild(gridCell);
+
