23c53a3d3f0cbb09a6d1db915e85c7b8b6293bda
Tolu A <mrtalder@gmail.com>
2024-04-07 09:49:26 +0100

    Update files to be able to mouseover grid cells and colour in each cell

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 const grid = document.querySelector('.grid');
-const gridCell = document.createElement('div');
+const gridSize = 400;
+let cellSize = 16;
 
-gridCell.style.width = '16px';
-gridCell.style.height = '16px';
-gridCell.style.borderStyle = 'solid';
-gridCell.style.borderColor = '#909090';
-gridCell.style.borderWidth = '1px';
+// Grid Styles
+grid.style.display = 'flex';
+grid.style.flexWrap = 'wrap';
+grid.style.width = '401px'; 
+grid.style.height = '401px'; 
+// To avoid double borders in the grid
+grid.style.borderLeft = '1px solid #909090';
+grid.style.borderTop = '1px solid #909090';
 
-grid.appendChild(gridCell);
+
+// Grid Cell Styles
+function createCell(gridCell, cellSize){
+    gridCell.style.width = `${cellSize}px`;
+    gridCell.style.height = `${cellSize}px`;
+    // To avoid double borders in the grid
+    gridCell.style.borderRight = '1px solid #909090';
+    gridCell.style.borderBottom = '1px solid #909090';
+}
+
+const cellsPerSide = gridSize/cellSize;
+gridArea = cellsPerSide * cellsPerSide;
+
+for (let i=0; i < gridArea; i++) {
+    const gridCell = document.createElement('div');
+    createCell(gridCell, cellSize);
+    grid.appendChild(gridCell);
+}
+
+// Mouse over grid cells
+const allGridCells = document.querySelectorAll('.grid > div');;
+for (const gridCell of allGridCells) {
+    gridCell.addEventListener("mouseover", ()=>{
+        gridCell.style.backgroundColor = '#000000'
+    })
+}
+
 
